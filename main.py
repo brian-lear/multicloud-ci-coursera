93227382e5d5d8234ee40dda8c4dc23b47773135
Brian Lear <brian.lear@invisto.com.br>
2023-03-30 10:53:58 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+def current_timestamp():
+    now = datetime.datetime.now()
+    return now
+
+
+def timestamp_to_binary(timestamp=None):
+    str_timestamp = str(timestamp)
+    if len(str_timestamp) == 0:
+        timestamp = current_timestamp()
+        str_timestamp = str(timestamp)
+
+    # printing original string
+    print("The original string is : " + str(str_timestamp))
+
+    # using join() + bytearray() + format()
+    # Converting String to binary
+    res = "".join(format(i, "08b") for i in bytearray(str_timestamp, encoding="utf-8"))
+
+    # printing result
+    print("The string after binary conversion : " + str(res))
+
+    return str(res)
+
+
+timestamp_to_binary()
